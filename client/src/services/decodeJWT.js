a48cb39fe1defb462f4c308e9225758022a97e0a
DivyaNareshkumarPatel <divyanareshkumarpatel@gmail.com>
2024-07-26 23:20:22 +0530

    token generated for users

--- client/src/services/decodeJWT.js
+++ client/src/services/decodeJWT.js
@@ -4,7 +4,7 @@ const getUserIdFromToken = () => {
   const token = localStorage.getItem('token'); // Adjust according to your token storage method
   if (token) {
     const decodedToken = jwtDecode(token);
-    return decodedToken.id; // Adjust according to your token structure
+    return decodedToken.id;
   }
   return null;
 };
