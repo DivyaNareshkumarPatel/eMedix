d9282bb983ad3fc8ed8ebaa0878a90257d76524a
DivyaNareshkumarPatel <divyanareshkumarpatel@gmail.com>
2024-07-30 12:01:11 +0530

    small changes

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+// app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use('/api/users', userRoutes);
 app.use('/api/admin', adminRoutes);
