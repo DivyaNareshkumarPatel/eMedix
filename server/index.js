7ee69730efea512e493d12637d92dc0f9bee7220
Divya Patel <121277699+DivyaNareshkumarPatel@users.noreply.github.com>
2024-07-30 13:55:47 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,9 @@ const appointmentRoutes = require('./routes/appointmentRouter.js');
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+app.use(cors({
+  origin: 'https://e-medix-git-master-divyas-projects-34cc0383.vercel.app' // Replace with your actual frontend URL
+}));
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
